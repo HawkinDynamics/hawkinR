1ba23cd3b470c1b37e84db5e6f25fef9f69b4687
Lauren Green <lauren@hawkindynamics.com>
2025-01-07 15:31:21 -0800

    removal of  function and docs. Update to

--- R/MetricDictionary.R
+++ R/MetricDictionary.R
@@ -6,9 +6,10 @@
 #' \describe{
 #'   \item{canonicalTestTypeId}{The unique id of the canonical test type (character).}
 #'   \item{testTypeName}{The given / common name of the test type (character).}
-#'   \item{id}{CamelCase formatted name of the metric (character).}
+#'   \item{id}{PascalCase formatted name of the metric (character).}
 #'   \item{label}{The metric label found in app and cloud UI (character).}
 #'   \item{label_unit}{The metric name returned from the API (character).}
+#'   \item{header}{header of data frame output (character).}
 #'   \item{units}{Metric unit of measure (character).}
 #'   \item{description}{A verbose description of the metric (character).}
 #' }
